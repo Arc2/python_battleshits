1ce8e167c233b6d3db27973da90319fa60e2ba90
Artur H. Lange <ArturLange@poczta.fm>
2019-06-27 16:53:20 +0200

    Add Field class

--- board.py
+++ board.py
@@ -1,10 +1,26 @@
+from dataclasses import dataclass
+from enum import Enum, auto
 from typing import List
 
 
+@dataclass
+class Field:
+    hit: bool = False
+    has_ship: bool = False
+    can_have_ship: bool = True
+
+    def __str__(self):
+        if self.hit:
+            return '*' if self.has_ship else 'X'
+        else:
+            return 'O'
+
+
 class Board:
-    def __init__(self, size):
+    def __init__(self, size: int):
         self.size = size
-        self.fields: List[List[str]] = [["O"] * size for x in range(size)]
+        self.fields: List[List[Field]] = [
+            [Field()] * size for x in range(size)]
 
     def print(self):
         a = '     '
@@ -18,7 +34,7 @@ class Board:
         j = 1
         for row in self.fields:
             if j < 10:
-                print(f'{str(j)}  | {" ".join(row)}')
+                print(f'{str(j)}  | {" ".join((str(x) for x in row))}')
             else:
-                print(f'{str(j)} | {" ".join(row)}')
+                print(f'{str(j)} | {" ".join((str(x) for x in row))}')
             j += 1
