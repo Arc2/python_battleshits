3330392bdedf37c93f72a4a18748d1fcd5018020
Artur H. Lange <ArturLange@poczta.fm>
2019-06-27 16:11:58 +0200

    Increase padding, use f-strings

--- board.py
+++ board.py
@@ -3,21 +3,22 @@ from typing import List
 
 class Board:
     def __init__(self, size):
+        self.size = size
         self.fields: List[List[str]] = [["O"] * size for x in range(size)]
 
     def print(self):
-        a = '*  '
-        for i in range(len(self.fields[0])):
-            a += chr(i+65) + ' '
+        a = '     '
+        for i in range(self.size):
+            a += f'{chr(i+65)} '
         print(a)
-        s = '*+'
-        for i in range(len(self.fields[0])):
+        s = '   +'
+        for i in range(self.size):
             s += '--'
         print(s)
         j = 1
         for row in self.fields:
             if j < 10:
-                print(str(j) + ' |' + " ".join(row))
+                print(f'{str(j)}  | {" ".join(row)}')
             else:
-                print(str(j) + '|' + " ".join(row))
+                print(f'{str(j)} | {" ".join(row)}')
             j += 1
